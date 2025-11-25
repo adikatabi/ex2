b7b314b07d195a5a53d2c3ec94d825e0c90aba0e
adikatabi <adikatabi@gmail.com>
2025-11-25 21:16:55 +0200

    final commit

--- ex2.c
+++ ex2.c
@@ -8,23 +8,25 @@ Assignment: ex2
 
 int main()
 {
-    printf("Welcome to our games, please choose an option:\n"
-           "1. Ducky's Unity Game\n"
-           "2. The Memory Game\n"
-           "3. Professor Pat's Power Calculation\n"
-           "4. The Duck Parade\n"
-           "5. The Mystery of the Repeated Digits\n"
-           "6. Good Night Ducks\n");
-    // MAIN MENU LOOP
     int choice;
-    scanf(" %d", &choice);
-    while ((choice < 1 || choice > 6)) {
-        printf("Invalid number. please try again\n");
+    const int TEN = 10;
+    do {    
+        printf("Welcome to our games, please choose an option:\n"
+            "1. Ducky's Unity Game\n"
+            "2. The Memory Game\n"
+            "3. Professor Pat's Power Calculation\n"
+            "4. The Duck Parade\n"
+            "5. The Mystery of the Repeated Digits\n"
+            "6. Good Night Ducks\n");
+        // MAIN MENU LOOP
         scanf(" %d", &choice);
-    }   
-    while(choice !=6){    
+        /*while ((choice < 1 || choice > 6)) {
+            printf("Invalid number. please try again\n");
+            scanf(" %d", &choice);
+        }   */
         switch(choice){
             case 1:
+            {
                 // TASK 1: Ducky's Unity Game code here
                 int dec_num, counter_one = 0, div = 1, rest;
                 printf("please enter a positive number:\n");
@@ -43,6 +45,7 @@ int main()
                 }
                 printf("Ducky earns %d corns\n", counter_one);
                 break;
+            }
             case 2:
                 // TASK 2: The Memory Game code here
                 int num_of_ducks, quak_or_sh, do_in_j;
@@ -109,8 +112,8 @@ int main()
                     printf("Invalid number, please try again\n");
                     scanf("%d", &number_of_ducks);
                 }
-                for(i= 0; i<=number_of_ducks/10; i++){
-                    if(number_of_ducks - counter_ducks <10){
+                for(i= 0; i<=number_of_ducks/TEN; i++){
+                    if(number_of_ducks - counter_ducks <TEN){
                         for(l= 0; l<number_of_ducks - counter_ducks; l++){ 
                             printf("   _\t\t");
                         }
@@ -122,24 +125,24 @@ int main()
                             for(d= 0; d<number_of_ducks - counter_ducks; d++){
                             printf("\\___)\t\t");
                         }
-                        if(number_of_ducks == 10){
+                        if(number_of_ducks == TEN){
                             break;
                         }
                     }
                     
                     else {
-                        for(l= 0; l<10; l++){ 
+                        for(l= 0; l<TEN; l++){ 
                             printf("   _\t\t");
                             }
                         printf("\n");
-                        for(j= 0; j<10; j++){
+                        for(j= 0; j<TEN; j++){
                             printf("__(o)>\t\t");}
                         printf("\n");
-                        for(d= 0; d<10; d++){
+                        for(d= 0; d<TEN; d++){
                             printf("\\___)\t\t");
                             }
                          printf("\n");
-                    counter_ducks = counter_ducks +10;
+                    counter_ducks = counter_ducks +TEN;
                     }
 
                 }
@@ -157,35 +160,33 @@ int main()
                     }
                 
                 while(numdig>0){
-                    digit = numdig %10;
-                    new_num = numdig/10;
+                    digit = numdig %TEN;
+                    new_num = numdig/TEN;
                     while(new_num>0){
-                        if(digit == new_num%10){
+                        if(digit == new_num%TEN){
                             printf("%d appears more than once!\n", digit);
                             break;
                         }
-                        else new_num = new_num/10;
+                        else new_num = new_num/TEN;
    
                     }
-                    numdig=numdig/10;
+                    numdig=numdig/TEN;
                 }
                 printf("\n");
                 break;
+
+                case 6:
+                printf("Good night! See you at the pond tomorrow.\n");
+                break;
+
+                default:
+                printf("Invalid option, please try again\n");
+                break;
+                
         }
-            printf("Welcome to our games, please choose an option:\n"
-           "1. Ducky's Unity Game\n"
-           "2. The Memory Game\n"
-           "3. Professor Pat's Power Calculation\n"
-           "4. The Duck Parade\n"
-           "5. The Mystery of the Repeated Digits\n"
-           "6. Good Night Ducks\n");        scanf("%d", &choice);
-        while ((choice < 1) || (choice > 6)) {
-            printf("Invalid number. please try again\n");
-            scanf("%d", &choice);
-        }  
     }
+    while(choice !=6);
     // TASK 6: EXIT
-    printf("Good night! See you at the pond tomorrow.\n");
 
 
 }
